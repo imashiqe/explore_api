a55cc7c8a9a0005ad05f6c10da58aeb8c06672e0
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-28 12:28:59 +0600

    json load data

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,15 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json()) //.json is  not similer but close to JSON .parse
-  .then(json => console.log(json))
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then(response => response.json()) //.json is  not similer but close to JSON .parse
+//   .then(json => console.log(json))
 
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+// fetch(url)
+// .then(response => response.json() )
+// .then(json => console.log(json))
+
+function loadData(){
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    fetch(url)
+.then(response => response.json() )
+.then(json => console.log(json))
+}
