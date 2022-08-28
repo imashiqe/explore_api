a2b64b728939832b557d9f28dcdca3816b7e2703
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-28 15:05:25 +0600

    data load json

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function loadData2(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(res => res.json())
+    .then(data => console.log(data));
+
+}
+
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
+
+function displayUsers(data){
+    console.log(data);
+}
