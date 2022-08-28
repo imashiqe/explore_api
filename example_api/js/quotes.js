450fc1b5f55f5ac0e53df57a594789e523e0278d
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-29 00:19:04 +0600

    kanye api quotes intregation

---
+++ example_api/js/quotes.js
@@ -0,0 +1,12 @@
+const loadQuote = () => {
+    fetch('https://api.kanye.rest/')
+    .then(res => res.json())
+    .then(data => displayQuote(data))
+}
+
+
+const displayQuote = quote => {
+    const blockQuote = document.getElementById('quote');
+
+    blockQuote.innerText = quote.quote;
+}
