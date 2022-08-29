c33fb8611ee5d81f09a90205a87fb89d8018c9f6
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-29 22:25:05 +0600

    api practice problem

---
+++ example_api/js/practice.js
@@ -0,0 +1,40 @@
+/*
+1. arrow function 
+2. map, forEach, filter , find
+3. template string
+4. API-- fetch
+5. DOM manipulation (recap)
+*/
+
+/*
+PRACTICE
+
+1. load comments and display comment on ui
+2. display 20 comments only (first 20)
+3. add a click handler on the comment:
+4. if you click on the comment, it will open the relevant post details
+
+Practice 2
+
+1. load data for random user
+2. display image 
+3. city, street,
+
+practice 3
+
+practice mealdb
+sports db
+
+Practice-4
+
+1. explore sports db
+2. display something
+3. dynamic API url
+4.  add handler  to display  something by loading data
+
+PRactice -5 
+
+1. explore network tab
+2. 
+
+*/
